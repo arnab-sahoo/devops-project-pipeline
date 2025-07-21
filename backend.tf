200a8afe13889c541819659d9f3ade224d3a69b2
arnab-sahoo <sahooarnab2025@gmail.com>
2025-07-21 12:01:01 +0530

    Changes committed for the DevOps Masters Project

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "my-codepipeline-bucket-254"
     key     = "state/terraform.tfstate"
-    region  = "ap-south-1a"
+    region  = "ap-south-1"
     encrypt = true # changes done
   }
 } 
